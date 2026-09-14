5c01353c35d595f8414bccacb32fa6c4ad960023
Wan-205 <hothetan05@gmail.com>
2026-09-14 18:54:49 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
+import React from "react";
 import { Link, Routes, Route } from "react-router-dom";
-
 import Navbar from "./components/Navbar";
 import Subjects from "./pages/Subjects";
 import Materials from "./pages/Materials";
